7693f7ff01ea533ee5794896ea7eba6ef3b00f43
Christiaan Bloemendaal <github@tnrd.net>
2022-07-30 22:35:57 +0200

    feat: added bepinex plugin loader

---
+++ Events/PluginLoader.cs
@@ -0,0 +1,13 @@
+﻿using BepInEx;
+
+namespace TNRD.Zeepkist
+{
+    [BepInPlugin("net.tnrd.zeepkist.events", "net.tnrd.zeepkist.events", "1.1.0")]
+    public class PluginLoader : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo("Plugin net.tnrd.zeepkist.events is loaded!");
+        }
+    }
+}
